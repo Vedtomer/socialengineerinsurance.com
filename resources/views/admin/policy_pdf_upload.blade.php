fb29e05a6cc319b780b8ac75f5fbb3608cc587d6
Ved <vedtomer5592@gmail.com>
2024-05-28 22:16:07 +0530

    fixed

--- resources/views/admin/policy_pdf_upload.blade.php
+++ resources/views/admin/policy_pdf_upload.blade.php
@@ -1,67 +1,81 @@
 @extends('admin.layouts.app')
 @section('title', 'Upload Multiple Policy PDF Files')
-@section('section')
+@section('content')
 
-    @if(session('success'))
-        <div class="alert alert-success mt-3" role="alert">
-            {{ session('success') }}
-        </div>
-    @endif
-    @if($errors->any())
-        <div class="alert alert-danger mt-3" role="alert">
-            <ul class="mb-0">
-                @foreach($errors->all() as $error)
+
+
+<div class="layout-px-spacing">
+
+    <div class="middle-content container-xxl p-0">
+
+
+        <div class="col-lg-6 mx-auto mt-4">
+
+            @if(session('success'))
+            <div class="alert alert-success mt-3" role="alert">
+                {{ session('success') }}
+            </div>
+            @endif
+            @if($errors->any())
+            <div class="alert alert-danger mt-3" role="alert">
+                <ul class="mb-0">
+                    @foreach($errors->all() as $error)
                     <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+                    @endforeach
+                </ul>
+            </div>
+            @endif
 
-    <div>
-        @if(!empty($successFiles))
-            <h3>Uploaded Successfully:</h3>
-            <ul>
-                @foreach($successFiles as $file)
+            <div>
+                @if(!empty($successFiles))
+                <h3>Uploaded Successfully:</h3>
+                <ul>
+                    @foreach($successFiles as $file)
                     <li>{{ $file }}</li>
-                @endforeach
-            </ul>
-        @endif
-    
-        @if(!empty($failedFiles))
-            <h3>Failed to Upload:</h3>
-            <ul>
-                @foreach($failedFiles as $file => $message)
-                    <li>{{ $file }} - {{ $message }}</li>
-                @endforeach
-            </ul>
-        @endif
-    </div>
+                    @endforeach
+                </ul>
+                @endif
 
-    <div class="container">
-        <div class="col-lg-6 d-flex mx-auto">
-            <div class="main-card mb-3 card mx-auto">
+                @if(!empty($failedFiles))
+                <h3>Failed to Upload:</h3>
+                <ul>
+                    @foreach($failedFiles as $file => $message)
+                    <li>{{ $file }} - {{ $message }}</li>
+                    @endforeach
+                </ul>
+                @endif
+            </div>
+
+            <div class="main-card mb-3 card">
                 <div class="card-body">
-                    <div class="add" style="display: flex; align-items: center;">
-                        <div class="btns" style="margin-left: auto;"></div>
+                    <div class="errors">
+                        @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                        <div class="alert alert-danger">
+                            {{ $error }}
+                        </div>
+                        @endforeach
+                        @endif
                     </div>
-        
-                    <form method="POST" action="{{ route('admin.policy_pdf_upload') }}" enctype="multipart/form-data">
+                    <form action="{{ route('sliders.store') }}" method="post" autocomplete="off" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3">
-                            <label class="form-label">Upload PDF</label>
-                            <input type="file" class="form-control" name="files[]" id="excelFile" accept=".pdf" multiple>
-                            <small class="text-muted">Accepted file types: PDF</small>
-                            @error('excelFile')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
+                            <label for="name">Upload Slider Image</label>
+                            <input type="file" class="form-control" name="image" required>
                         </div>
-                        <button type="submit" class="btn btn-primary">Upload</button>
+
+
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <a href="{{ route('agent.list') }}" class="btn btn-secondary">Back</a>
+                        </div>
                     </form>
-                    
-                
                 </div>
             </div>
         </div>
+
     </div>
+
+</div>
 
 @endsection
--- resources/views/admin/sliders/create.blade.php
+++ resources/views/admin/sliders/create.blade.php
@@ -2,33 +2,43 @@
 
 @section('content')
 
-    <div class="col-lg-6 mx-auto mt-4">
-        <div class="main-card mb-3 card">
-            <div class="card-body">
-                <div class="errors">
-                    @if ($errors->any())
-                        @foreach ($errors->all() as $error)
-                            <div class="alert alert-danger">
-                                {{ $error }}
+    
+
+
+    <div class="layout-px-spacing">
+
+        <div class="middle-content container-xxl p-0">
+    
+            <div class="col-lg-6 mx-auto mt-4">
+                <div class="main-card mb-3 card">
+                    <div class="card-body">
+                        <div class="errors">
+                            @if ($errors->any())
+                                @foreach ($errors->all() as $error)
+                                    <div class="alert alert-danger">
+                                        {{ $error }}
+                                    </div>
+                                @endforeach
+                            @endif
+                        </div>
+                        <form action="{{ route('sliders.store') }}" method="post" autocomplete="off" enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="name">Upload Slider Image</label>
+                                <input type="file" class="form-control" name="image" required>
                             </div>
-                        @endforeach
-                    @endif
+        
+        
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <a href="{{ route('agent.list') }}" class="btn btn-secondary">Back</a>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <form action="{{ route('sliders.store') }}" method="post" autocomplete="off" enctype="multipart/form-data">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="name">Upload Slider Image</label>
-                        <input type="file" class="form-control" name="image" required>
-                    </div>
-
-
-                    <div class="text-center">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        <a href="{{ route('agent.list') }}" class="btn btn-secondary">Back</a>
-                    </div>
-                </form>
             </div>
+    
         </div>
+    
     </div>
-
 @endsection
