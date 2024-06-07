d07d0204dbde5f4fed7844a4fcde6f3e4870cd7c
Ved <vedtomer5592@gmail.com>
2024-06-08 03:42:04 +0530

    fixed

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -8,7 +8,9 @@
         </a> --}}
 
          <a href="javascript:void(0);" class="sidebarCollapse">
-             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="feather feather-menu">
                  <line x1="3" y1="12" x2="21" y2="12"></line>
                  <line x1="3" y1="6" x2="21" y2="6"></line>
                  <line x1="3" y1="18" x2="21" y2="18"></line>
@@ -29,22 +31,16 @@
             </form>
         </div> --}}
 
-        <div >
-            <select id="select-beast" placeholder="Select a Agent" autocomplete="off">
-                <option >Select Agent</option>
-@foreach ($data['agent'] as $item)
-<option value="{{$item['id']}}">{{$item['name']}}</option>
-@endforeach
-            </select>
-        </div>
+
 
          <ul class="navbar-item flex-row ms-lg-auto ms-0">
 
              <li class="nav-item dropdown user-profile-dropdown  order-lg-0 order-1">
-                 <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown"
+                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                      <div class="avatar-container">
                          <div class="avatar avatar-sm avatar-indicators avatar-online">
-                             <img alt="avatar" src="{{Auth::user()->profile_image}}" class="rounded-circle">
+                             <img alt="avatar" src="{{ Auth::user()->profile_image }}" class="rounded-circle">
                          </div>
                      </div>
                  </a>
@@ -52,7 +48,7 @@
                  <div class="dropdown-menu position-absolute" aria-labelledby="userProfileDropdown">
                      <div class="user-profile-section">
                          <div class="media mx-auto">
-                             <img src="{{Auth::user()->profile_image}}" class="img-fluid me-2" alt="avatar">
+                             <img src="{{ Auth::user()->profile_image }}" class="img-fluid me-2" alt="avatar">
                              <div class="media-body">
                                  <h5>Admin</h5>
 
@@ -60,8 +56,10 @@
                          </div>
                      </div>
                      <div class="dropdown-item">
-                         <a href="{{route('admin.profile')}}">
-                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                         <a href="{{ route('admin.profile') }}">
+                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round" class="feather feather-user">
                                  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                                  <circle cx="12" cy="7" r="4"></circle>
                              </svg> <span>Profile</span>
@@ -82,36 +80,51 @@
 
 
                      <div class="dropdown-item">
-                        <a href="javascript:void(0);" class="nav-link theme-toggle ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon dark-mode">
-                                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun light-mode">
-                                <circle cx="12" cy="12" r="5"></circle>
-                                <line x1="12" y1="1" x2="12" y2="3"></line>
-                                <line x1="12" y1="21" x2="12" y2="23"></line>
-                                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-                                <line x1="1" y1="12" x2="3" y2="12"></line>
-                                <line x1="21" y1="12" x2="23" y2="12"></line>
-                                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                            </svg> Change Theme
-                        </a>
-                    </div>
-                    <div class="dropdown-item">
-                        <a href="{{route("admin.logout")}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
-                                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-                                <polyline points="16 17 21 12 16 7"></polyline>
-                                <line x1="21" y1="12" x2="9" y2="12"></line>
-                            </svg> <span>Log Out</span>
-                        </a>
-                    </div>
+                         <a href="javascript:void(0);" class="nav-link theme-toggle ">
+                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round" class="feather feather-moon dark-mode">
+                                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                             </svg>
+                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round" class="feather feather-sun light-mode">
+                                 <circle cx="12" cy="12" r="5"></circle>
+                                 <line x1="12" y1="1" x2="12" y2="3"></line>
+                                 <line x1="12" y1="21" x2="12" y2="23"></line>
+                                 <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                                 <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                                 <line x1="1" y1="12" x2="3" y2="12"></line>
+                                 <line x1="21" y1="12" x2="23" y2="12"></line>
+                                 <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                                 <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                             </svg> Change Theme
+                         </a>
+                     </div>
+                     <div class="dropdown-item">
+                         <a href="{{ route('admin.logout') }}">
+                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                 stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
+                                 <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                                 <polyline points="16 17 21 12 16 7"></polyline>
+                                 <line x1="21" y1="12" x2="9" y2="12"></line>
+                             </svg> <span>Log Out</span>
+                         </a>
+                     </div>
                  </div>
 
              </li>
          </ul>
+
+         <div>
+            <select id="select-beast" placeholder="Select a Agent" autocomplete="off">
+                <option>Select Agent</option>
+                @foreach ($data['agent'] as $item)
+                    <option value="{{ $item['id'] }}">{{ $item['name'] }}</option>
+                @endforeach
+            </select>
+        </div>
      </header>
  </div>
  <!--  END NAVBAR  -->
