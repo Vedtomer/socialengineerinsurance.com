42fbddd91f70fb0e26b826f37e42448540287377
Ved <vedtomer5592@gmail.com>
2024-06-23 01:36:48 +0530

    fixed

--- app/Models/CustomerPolicy.php
+++ app/Models/CustomerPolicy.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class CustomerPolicy extends Model
 {
     use HasFactory;
-
+    protected $appends = ['policy_link'];
     protected $table = 'customer_policies';
 
     protected $fillable = [
@@ -28,4 +29,16 @@ class CustomerPolicy extends Model
         'policy_start_date' => 'date',
         'policy_end_date' => 'date',
     ];
+
+    public function getPolicyLinkAttribute()
+    {
+
+        $data = ('/customer_policies') . "/" . $this->policy_no . '.pdf';
+
+        if (Storage::disk('public')->exists($data)) {
+            return asset('/storage/customer_policies') . "/" . $this->policy_no . '.pdf';
+        } else {
+            return "";
+        }
+    }
 }
