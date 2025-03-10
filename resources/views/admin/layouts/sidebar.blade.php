fd5ab16fd91032f548472cc78b27201bb8388d11
Ved Tomer <vedtomer@gmail.com>
2025-03-10 16:45:48 +0530

    fix

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -292,7 +292,7 @@
 
 
             <li class="menu {{ classActivePath('app-activity') }}">
-                <a href="{{ route('app-activity') }}" aria-expanded="false" class="dropdown-toggle">
+                <a href="{{ route('admin.app-activity') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
