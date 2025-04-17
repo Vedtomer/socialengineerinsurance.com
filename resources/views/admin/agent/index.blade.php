ec48503b602f405cc2f1c666a3084fc515870994
Ved Tomer <vedtomer@gmail.com>
2025-04-17 22:02:07 +0530

    fix

--- resources/views/admin/agent/index.blade.php
+++ resources/views/admin/agent/index.blade.php
@@ -131,9 +131,7 @@
                                                                         <div class="modal-header">
                                                                             <h5 class="modal-title">PAN Card -
                                                                                 {{ $agent->name }}</h5>
-                                                                            <button type="button" class="btn-close"
-                                                                                data-bs-dismiss="modal"
-                                                                                aria-label="Close"></button>
+                                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="clearModal()"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
                                                                         </div>
                                                                         <div class="modal-body text-center">
                                                                             <img src="{{ asset('storage/pan_images/' . $agent->pan_image) }}"
